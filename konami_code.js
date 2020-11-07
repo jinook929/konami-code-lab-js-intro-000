ccc2e7ac7237ad4f1a3c225f34af789d636ea27b
JINOOK JUNG <jinook929@gmail.com>
2020-11-07 23:45:24 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -13,11 +13,11 @@ const codes = [
 
 function init() {
   let key;
-  document.body.addEventListener("keydown", e => {
-    key = e.key;
+  document.body.addEventListener("keydown", e1 => {
+    key = e1.key;
     if(key === "ArrowUp") {
-      document.body.addEventListener("keydown", e => {
-        key = e.key;
+      document.body.addEventListener("keydown", e2 => {
+        key = e2.key;
         if(key === "ArrowDown") {
           alert("Arrow Up and Down !!!");
         }
